7025ac7658f343bdd8794ab4d3ec1ee4ea269820
Kim Anh Do <kim@Kims-MBP.fritz.box>
2023-12-19 14:41:41 +0100

    Setup Frondend, installed react-vite and tailwindcss

---
+++ Frontend/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "react-app",
+    "react-app/jest",
+    "plugin:jsx-a11y/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "jsx-a11y"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/prop-types": "off",
+  },
+};
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Chat App</h1>
+    </>
+  );
+}
+
+export default App;
