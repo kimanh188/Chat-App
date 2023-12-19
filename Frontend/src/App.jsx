a86f85588ba9e414d77484b0d0243efff89ccae4
Kim Anh Do <kim@Kims-MBP.fritz.box>
2023-12-19 23:47:20 +0100

    Installed react-router-dom & Helmet

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,28 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import { LoginComponent } from "./components/Login/loginComponent.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path={"/"}>
+        <Route index element={<LoginComponent />} />
+      </Route>
+    </>
+  )
+);
 
 function App() {
   return (
     <>
-      <LoginComponent />
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
     </>
   );
 }
