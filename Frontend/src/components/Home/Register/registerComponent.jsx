866d53fe83ab0c3a9b39f8f66280cda3fdd5d58a
Kim Anh Do <kim@Kims-MBP.fritz.box>
2023-12-20 01:51:03 +0100

    Updated homepage

--- Frontend/src/components/Home/Register/registerComponent.jsx
+++ Frontend/src/components/Home/Register/registerComponent.jsx
@@ -1,20 +1,20 @@
 export function RegisterComponent() {
   return (
-    <form action="" className="w-2/3 max-w-2xl">
+    <form action="" className="tab-content w-full">
       <input
         type="email"
         placeholder="Email"
-        className="block w-full p-2 mb-2 rounded-sm border"
+        className="block w-full p-2 mb-2 rounded-sm input-bordered input-info"
       />
       <input
         type="text"
         placeholder="Username"
-        className="block w-full p-2 mb-2 rounded-sm border"
+        className="block w-full p-2 mb-2 rounded-sm input-bordered input-info"
       />
       <input
         type="password"
         placeholder="Password"
-        className="block w-full p-2 mb-2 rounded-sm border"
+        className="block w-full p-2 mb-2 rounded-sm input-bordered input-info"
       />
       <button className="bg-blue-500 text-white block w-full p-2 rounded-md">
         Register
--- Frontend/src/components/Home/homepageComponent.jsx
+++ Frontend/src/components/Home/homepageComponent.jsx
@@ -1,39 +1,43 @@
+import { useState } from "react";
 import { LoginComponent } from "./Login/loginComponent.jsx";
 import { RegisterComponent } from "./Register/registerComponent.jsx";
 import "./homepage.style.css";
 
 export function HomepageComponent() {
+  const [activeTab, setActiveTab] = useState("login");
+  const tabChangeHandler = (event) => {
+    setActiveTab(event.target.name);
+    console.log(event.target.name);
+  };
+
   return (
     <>
       <div className="bg-blue-200 h-screen flex flex-col items-center pt-20">
         <h1 className="text-blue-800 text-3xl m-5"> Let the chatting begin!</h1>
 
-        <div role="tablist" className="tabs tabs-bordered w-2/3 max-w-2xl ">
+        <div role="tablist" className="tabs tabs-bordered">
           <input
             type="radio"
-            name="my_tabs_1"
+            name="login-tab"
             role="tab"
             className="tab"
             aria-label="Login"
+            checked={activeTab === "login-tab"}
+            onChange={tabChangeHandler}
           />
-          <div role="tabpanel" className="tab-content ">
-            <LoginComponent />
-          </div>
+          <LoginComponent />
 
           <input
             type="radio"
-            name="my_tabs_1"
+            name="register-tab"
             role="tab"
             className="tab"
             aria-label="Register"
-            checked
+            checked={activeTab === "register-tab"}
+            onChange={tabChangeHandler}
           />
-          <div role="tabpanel" className="tab-content">
-            <RegisterComponent />
-          </div>
+          <RegisterComponent />
         </div>
-
-        {/*  */}
       </div>
     </>
   );
