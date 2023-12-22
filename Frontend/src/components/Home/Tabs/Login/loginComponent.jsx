de53c8ad099e997672c89c5d07360cfb1bfc0753
Kim Anh Do <kim@Kims-MBP.fritz.box>
2023-12-22 11:25:00 +0100

    Updated password field - added show/hide option

--- Frontend/src/components/Home/Tabs/Login/loginComponent.jsx
+++ Frontend/src/components/Home/Tabs/Login/loginComponent.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 export function LoginComponent() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onClickHandler = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <form action="" className="w-full">
       <label htmlFor="email" className="block mb-2 pl-2 font-bold">
@@ -6,7 +14,7 @@ export function LoginComponent() {
       </label>
       <input
         type="email"
-        id="email"
+        id="login-email"
         name="email"
         placeholder="Your email"
         className="block w-full p-2 mb-2 rounded-sm input-bordered input-info"
@@ -15,17 +23,31 @@ export function LoginComponent() {
       <label htmlFor="password" className="block mb-2 pl-2 font-bold">
         Password *:
       </label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Your password"
-        className="block w-full p-2 mb-2 rounded-sm input-bordered input-info"
-      />
-      <button
-        type="submit"
-        className="bg-blue-500 text-white block w-full p-2 rounded-md font-bold hover:text-blue-500 hover:bg-blue-200"
-      >
+      <div className="relative">
+        <input
+          type={showPassword ? "text" : "password"}
+          id="login-password"
+          name="password"
+          placeholder="Secure your account with 8+ chars, 1 UPPERCASE"
+          required={true}
+          className="block w-full p-2 mb-2 rounded-sm input-bordered input-info bg-gray-50 focus:bg-white"
+        />
+        <button
+          className="absolute inset-y-0 right-3 flex items-center pr-2 cursor-pointer"
+          onClick={onClickHandler}
+        >
+          <img
+            src={
+              showPassword
+                ? "src/assets/eye-hide.png"
+                : "src/assets/eye-show.png"
+            }
+            alt={showPassword ? "hide eye-symbol" : "show eye-symbol"}
+          />
+        </button>
+      </div>
+
+      <button className="bg-blue-500 text-white block w-full p-2 rounded-md font-bold hover:text-blue-500 hover:bg-blue-200">
         Login
       </button>
     </form>
--- Frontend/src/components/Home/Tabs/Register/registerComponent.jsx
+++ Frontend/src/components/Home/Tabs/Register/registerComponent.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react";
+
 export function RegisterComponent() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [profileImg, setProfileImg] = useState();
+
+  const emailInputHandler = (event) => {
+    setEmail(event.target.value);
+    console.log(email);
+  };
+
+  const usernameInputHandler = (event) => {
+    setUsername(event.target.value);
+    console.log(username);
+  };
+
+  const passwordInputHandler = (event) => {
+    setPassword(event.target.value);
+    console.log(password);
+  };
+
+  const onClickHandler = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <form action="" className=" w-full">
       <label htmlFor="email" className="block mb-2 pl-2 font-bold">
@@ -10,7 +37,9 @@ export function RegisterComponent() {
         name="email"
         placeholder="yourMagicMail@example.com"
         required={true}
-        className="block w-full p-2 mb-2 rounded-sm input-bordered input-info"
+        value={email}
+        onChange={emailInputHandler}
+        className="block w-full p-2 mb-2 rounded-sm input-bordered input-info bg-gray-50 focus:bg-white"
       />
 
       <label htmlFor="username" className="block mb-2 pl-2 font-bold">
@@ -22,20 +51,39 @@ export function RegisterComponent() {
         name="username"
         placeholder="ChattingWizard"
         required={true}
-        className="block w-full p-2 mb-2 rounded-sm input-bordered input-info"
+        value={username}
+        onChange={usernameInputHandler}
+        className="block w-full p-2 mb-2 rounded-sm input-bordered input-info bg-gray-50 focus:bg-white"
       />
 
       <label htmlFor="password" className="block mb-2 pl-2 font-bold">
         Password *:
       </label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Secure your account with 8+ chars, 1 UPPERCASE"
-        required={true}
-        className="block w-full p-2 mb-2 rounded-sm input-bordered input-info"
-      />
+      <div className="relative">
+        <input
+          type={showPassword ? "text" : "password"}
+          id="password"
+          name="password"
+          placeholder="Secure your account with 8+ chars, 1 UPPERCASE"
+          required={true}
+          value={password}
+          onChange={passwordInputHandler}
+          className="block w-full p-2 mb-2 rounded-sm input-bordered input-info bg-gray-50 focus:bg-white"
+        />
+        <button
+          className="absolute inset-y-0 right-3 flex items-center pr-2 cursor-pointer"
+          onClick={onClickHandler}
+        >
+          <img
+            src={
+              showPassword
+                ? "src/assets/eye-hide.png"
+                : "src/assets/eye-show.png"
+            }
+            alt={showPassword ? "hide eye-symbol" : "show eye-symbol"}
+          />
+        </button>
+      </div>
 
       <label htmlFor="profileImg" className="block mb-2 pl-2 font-bold">
         Upload a profile image (optional):
