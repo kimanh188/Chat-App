39c1ccdf29c67991051167d31d84aa3100c7fda7
Kim Anh Do <kim@Kims-MBP.fritz.box>
2024-01-01 17:24:39 +0100

    Added welcome bar with users default avatar

--- Frontend/src/components/ChatPage/ChatBox/chatBoxComponent.jsx
+++ Frontend/src/components/ChatPage/ChatBox/chatBoxComponent.jsx
@@ -1,7 +1,7 @@
 export function ChatBoxComponent() {
   return (
-    <div className="w-2/3 h-screen bg-green-200">
-      <h1>Chat with a specific user here</h1>
+    <div className="w-2/3 h-screen bg-blue-100 px-5">
+      <h1 className="py-5">Chat with a specific user here</h1>
     </div>
   );
 }
--- Frontend/src/components/ChatPage/ChatList/chatListComponent.jsx
+++ Frontend/src/components/ChatPage/ChatList/chatListComponent.jsx
@@ -5,9 +5,18 @@ export function ChatListComponent() {
   const { loggedInUsername } = useContext(UserContext);
 
   return (
-    <div className="w-1/3 h-screen bg-yellow-200 pl-5">
-      <div>
-        <h1 className="py-5">Welcome {loggedInUsername}</h1>
+    <div className="w-1/3 h-screen bg-yellow-200 px-5">
+      <div className="flex justify-between content-center">
+        <h1 className="py-5 text-2xl inline-block">
+          Welcome {loggedInUsername}
+        </h1>
+        <div className="flex items-center inline-block">
+          <img
+            src="src/assets/default-user-avatar.svg"
+            alt="User Avatar"
+            className="bg-blue-200 rounded-full w-15 p-2"
+          />
+        </div>
       </div>
 
       <h2>All conversations with the latest message here</h2>
