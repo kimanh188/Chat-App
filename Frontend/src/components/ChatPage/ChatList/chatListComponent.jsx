ec9b6a26981b6dbe8948fddde9791704933d1dae
Kim Anh Do <kim@Kims-MBP.fritz.box>
2024-01-03 15:20:12 +0100

    Retrieved all conversations - small update

--- Frontend/src/components/ChatPage/ChatList/chatListComponent.jsx
+++ Frontend/src/components/ChatPage/ChatList/chatListComponent.jsx
@@ -52,12 +52,12 @@ export function ChatListComponent() {
   }, []);
 
   return (
-    <div className="w-1/3 h-screen bg-yellow-100 pl-5 text-gray-900">
+    <div className="w-1/3 h-screen bg-yellow-100 px-5 text-gray-900">
       <div className="flex justify-between items-center pt-5 ">
         <h1 className="text-2xl inline-block w-2/3">
           Welcome {loggedInUsername}!
         </h1>
-        <div className="flex items-center inline-block w-1/3">
+        <div className="flex items-center inline-block ">
           <img
             src={profileImgPath}
             alt="User Avatar"
@@ -65,13 +65,18 @@ export function ChatListComponent() {
           />
         </div>
       </div>
+
       <div>
-        <h2 className="pt-5">All conversations with the latest message here</h2>
+        <h2 className="py-5">All conversations with the latest message here</h2>
         <div>
           {conversations.map((conversation, index) => (
-            <div key={index}>
-              <h3>{conversation.conversationName}</h3>
-              <p>{conversation.messages[0].message}</p>
+            <div key={index} className="py-2 mb-4 bg-blue-300">
+              <h3 className="font-bold">{conversation.conversationName}</h3>
+              <div>
+                {conversation.messages.map((mes, index) => (
+                  <p key={index}>{mes.message}</p>
+                ))}
+              </div>
             </div>
           ))}
         </div>
