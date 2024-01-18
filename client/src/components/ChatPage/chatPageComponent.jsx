0342e5e56f4613f9dda92a837fc0ffb0e236d993
Kim Anh Do <kim@Kims-MBP.fritz.box>
2024-01-18 14:05:48 +0100

    updated

--- client/src/components/ChatPage/chatPageComponent.jsx
+++ client/src/components/ChatPage/chatPageComponent.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { SearchComponent } from "./SearchBar/searchComponent.jsx";
 import { UserProfileComponent } from "../UserProfile/userProfileComponent.jsx";
