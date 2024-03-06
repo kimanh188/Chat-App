30605d6ab627bdded70e67274edb29a63de437f9
kim <kimanh188.de@gmail.com>
2024-03-06 12:15:10 +0100

    Updated profile page

--- client/src/routes/UsersProfile/profileContainer.jsx
+++ client/src/routes/UsersProfile/profileContainer.jsx
@@ -1,7 +1,12 @@
 import { useContext, useEffect } from "react";
-/* import Cookies from "js-cookie";
-import axios from "axios"; */
+import axios from "axios";
+import Cookies from "js-cookie";
 import { UserContext } from "../../contexts/userContext.jsx";
+import EditIcon from "../../assets/edit.svg";
+import KeyIcon from "../../assets/key.svg";
+import InfoIcon from "../../assets/info.svg";
+import UpdateIcon from "../../assets/update.svg";
+import LogoutIcon from "../../assets/logout.svg";
 
 export function UserProfilePage() {
   //const [token, setToken] = useState("");
@@ -45,6 +50,28 @@ export function UserProfilePage() {
     }
   }; */
 
+  const logOutHandler = async () => {
+    try {
+      const response = await axios.post("http://localhost:3022/user/logout", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+
+      console.log("Response from logout: ", response);
+
+      //remove cookie jwt - temporary solution when figuring out why backend code doesn't remove the cookie
+      Cookies.remove("jwt");
+
+      localStorage.clear();
+
+      window.location.href = "/login";
+    } catch (error) {
+      console.log("Error logging out: ", error);
+    }
+  };
+
   useEffect(() => {
     if (loggedInUsername) {
       localStorage.setItem("storedUsername", loggedInUsername);
@@ -67,19 +94,88 @@ export function UserProfilePage() {
   }, []);
 
   return (
-    <>
-      <h1>{loggedInUsername || storedUsername}&apos;s Profile </h1>
-      <h2>Username: {loggedInUsername || storedUsername}</h2>
-      <h2>Email: {loggedInEmail || storedEmail}</h2>
-      <h2>UserId: {loggedInId || storedId}</h2>
-      <h2>Profile Image: </h2>
-      <img src={profileImgPath} alt="User Avatar" />
-      <button className="border border-indigo-600 rounded-md p-2 m-2">
-        Change Profile Image
-      </button>
-      <button className="border border-indigo-600 rounded-md p-2 m-2">
-        Change Password
-      </button>
-    </>
+    <div className="h-screen p-5 mx-auto max-w-4xl">
+      <h1 className="text-2xl pb-5">Setting</h1>
+
+      <div className="flex flex-col items-center justify-center ">
+        <div className="relative max-w-max">
+          <img
+            className="rounded-full w-36 h-36 border-8 p-1 border-purple-600"
+            src={profileImgPath}
+            alt="User Avatar"
+          />
+          <button>
+            <img
+              className="rounded-full border border-white bg-purple-300 p-2 absolute bottom-5 right-0"
+              src={EditIcon}
+              alt="edit icon"
+            />
+          </button>
+        </div>
+        <h2 className="pt-1 pb-8 text-2xl font-bold">
+          {loggedInUsername || storedUsername}
+        </h2>
+
+        <div className="flex flex-col gap-4 items-start ">
+          <button className="flex items-center justify-center gap-2 ">
+            <img
+              className="rounded-full border border-white bg-purple-400 p-2 bottom-5 right-0"
+              src={EditIcon}
+              alt="edit icon"
+            />
+            <h6 className="font-semibold text-xl hover:text-indigo-600 ">
+              Edit profile
+            </h6>
+          </button>
+
+          <button className="flex items-center justify-center gap-2">
+            <img
+              className="rounded-full border border-white bg-purple-400 p-2 bottom-5 right-0"
+              src={KeyIcon}
+              alt="key icon"
+            />
+            <h6 className="font-semibold text-xl hover:text-indigo-600 ">
+              Change password
+            </h6>
+          </button>
+
+          <button className="flex items-center justify-center gap-2">
+            <img
+              className="rounded-full border border-white bg-purple-400 p-2 bottom-5 right-0"
+              src={InfoIcon}
+              alt="info icon"
+            />
+            <h6 className="font-semibold text-xl hover:text-indigo-600 ">
+              Information
+            </h6>
+          </button>
+
+          <button className="flex items-center justify-center gap-2">
+            <img
+              className="rounded-full border border-white bg-purple-400 p-2 bottom-5 right-0"
+              src={UpdateIcon}
+              alt="update icon"
+            />
+            <h6 className="font-semibold text-xl hover:text-indigo-600 ">
+              Update
+            </h6>
+          </button>
+
+          <button
+            className="flex items-center justify-center gap-2"
+            onClick={logOutHandler}
+          >
+            <img
+              className="rounded-full border border-white bg-purple-400 p-2 bottom-5 right-0"
+              src={LogoutIcon}
+              alt="logout icon"
+            />
+            <h6 className="font-semibold text-xl hover:text-indigo-600 ">
+              Logout
+            </h6>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
