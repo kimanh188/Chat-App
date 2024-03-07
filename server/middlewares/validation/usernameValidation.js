e734b5e9ce945e0c26bf764cfc485fef264934f4
kim <kimanh188.de@gmail.com>
2024-03-07 12:38:49 +0100

    Updated username validation

--- server/middlewares/validation/usernameValidation.js
+++ server/middlewares/validation/usernameValidation.js
@@ -4,4 +4,7 @@ export const usernameValidation = check("username")
   .isLength({ min: 3 })
   .escape()
   .trim()
-  .withMessage("Username must be at least 3 characters long");
+  .matches(/^[a-zA-Z0-9]+$/) // only letters and numbers, no special characters or spaces
+  .withMessage(
+    "Username must be at least 3 characters long and contain only letters and numbers."
+  );
